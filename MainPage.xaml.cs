e63003cf3e386322eb3f13c7baf5b068617f6e5e
AnasKhraim20 <79458646+AnasKhraim20@users.noreply.github.com>
2022-04-02 22:24:14 +0300

    no message

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inforamtion
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Picker_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
